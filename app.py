c56cc0b5367766db74b80961a7477d28a8e9b3d7
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-04-26 11:45:02 +0200

    Update

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def webhook():
                               
     if data["object"] == "page":
 
-        for entry in data["entry"]:
+        for entry in data["entry"] = unicode(sys.argv[1], 'utf8'):
             if 'messaging' in entry:
                 for messaging_event in entry["messaging"]:
 
@@ -40,7 +40,6 @@ def webhook():
                         sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                         recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                         message_text = messaging_event["message"]["text"]  # the message's text
-                        message_text = unicode(sys.argv[1], 'utf8')
                         
                         for word in message_text.split():
                             if word.lower() in Velkomst_receive:
