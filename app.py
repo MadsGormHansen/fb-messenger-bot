01fd48972c5108c46589f93b49c906e91550bff9
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-04-26 14:12:42 +0200

    Update

--- app.py
+++ app.py
@@ -69,23 +69,23 @@ def velkomst_check(message_text):
     for word in message_text.split():
         if word.lower() in Velkomst_receive:
             return random.choice(Velkomst_send)
-    return hej
+    return message_text.split(","):
     
 def efter_velkomst(message_text):
-    for word1, word2 in message_text.split():
-        if word1.lower() in eftervelkomst_receive1 and word2.lower() in eftervelkomst_receive2:
+    for word in message_text.split(","):
+        if word.lower()  in eftervelkomst_receive1)  and word.lower() in eftervelkomst_receive2:
             return random.choice(eftervelkomst_send1)
-        if word1.lower() in eftervelkomst_receive1 and word2.lower() in eftervelkomst_receive3:
+        if word1.lower() in eftervelkomst_receive1 and word.lower() in eftervelkomst_receive3:
             return random.choice(eftervelkomst_send2)
-        if word1.lower() in eftervelkomst_receive1 and word2.lower() in eftervelkomst_receive4:
+        if word1.lower() in eftervelkomst_receive1 and word.lower() in eftervelkomst_receive4:
             return random.choice(eftervelkomst_send2)
-    return hej2
+    return message_text.split(",")
 
 def Send(message_text):
-    for word1,word2 in message_text.split():
-        if word1.lower() in Velkomst_receive:
+    for word in message_text.split():
+        if word.lower() in Velkomst_receive:
             return velkomst_check(message_text)
-        if word1.lower() in eftervelkomst_receive1 and word2.lower() in eftervelkomst_receive2 or eftervelkomst_receive3 or eftervelkomst_receive4:
+        if word.lower() in eftervelkomst_receive1 and word.lower() in eftervelkomst_receive2 or eftervelkomst_receive3 or eftervelkomst_receive4:
             return efter_velkomst(message_text)
     return "fuck dig!"
 
