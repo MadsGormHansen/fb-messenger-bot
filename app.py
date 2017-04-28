e661965a8efe2809a86a83efa751ee735380ce01
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-04-28 14:34:20 +0200

    Update

--- app.py
+++ app.py
@@ -45,11 +45,7 @@ def webhook():
                         reply_text = Send(message_text)
                         
                         send_message(sender_id, reply_text)
-                        
-                        def previous_messages(message_text,reply_text):
-                            for i,message_text,reply_text in enumerate(message_text,reply_text):
-                                listing.append(previous_messages(message_text,reply_text))
-                        
+                                                
                     if messaging_event.get("delivery"):  # delivery confirmation
                         pass
 
@@ -78,6 +74,10 @@ person_arbejde = ("medarbejder", "kollega", "teammate")
 person_foraeldre = ("mor", "far", "foraeldre")
 
 listing = []
+
+def previous_messages(message_text,reply_text):
+    for i,message_text,reply_text in enumerate(message_text,reply_text):
+        listing.append(previous_messages(message_text,reply_text))
 
 def velkomst_check(message_text):
     for word in message_text.split():
