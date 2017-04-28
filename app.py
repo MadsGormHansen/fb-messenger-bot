c92d99acf1a6e043e98896ab278e1a49ea38d11a
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-04-28 13:37:56 +0200

    Update

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ person_kaerlighed = ("kone", "kaereste")
 person_arbejde = ("medarbejder", "kollega", "teammate")
 person_foraeldre = ("mor", "far", "foraeldre")
 
-listing = [i for i in range(n)]
+listing = [i for i in range(100)]
 
 def velkomst_check(message_text):
     for word in message_text.split():
