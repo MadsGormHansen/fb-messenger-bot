1316afe154c17c6187d38e49dca64e0beaff4e22
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-05-01 15:06:52 +0200

    update

--- app.py
+++ app.py
@@ -9,11 +9,21 @@ from fbmq import Attachment, Template, QuickReply, Page
 
 page = fbmq.Page(EAAD5ZA0yoEewBAIh13Kg8HthCP5mBV90m9e2b98vLKTACbWGLD0ZAPlFzf7J4bgESHLp1XXyM45MP42z01AWZCB35Qhv6n0keaSGQZCxZCe3vAbd22tjwzHtgx3hgFRqs6kHCb757onZCtEWmqULI2aT5GMJtalSZBy90HvN7uYxgZDZD)
 
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
   page.handle_webhook(request.get_data(as_text=True))
   return "ok"
-
 
 @page.handle_message
 def message_handler(event):
@@ -21,7 +31,7 @@ def message_handler(event):
   sender_id = event.sender_id
   message = event.message_text
   
-  page.send(sender_id, "thank you! your message is '%s'" % message)
+  page.send(sender_id, "thank you! your message is")
 
 @page.after_send
 def after_send(payload, response):
