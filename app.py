2ac4e8107b983ac6f1a97115d130fe2464d0f609
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-05-09 11:41:57 +0200

    Update

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def send(message):
     person_detectblomstervar= person_detectblomster(message)
     person_detectalkoholvar = person_detectalkohol(message)
 
-    if listing == [] 
+    if listing == []:
         if first_trigger_var is 1:
             if eftervelkomstvar is 1 and person_detectblomstervar != "none":
                 return person_detectblomstervar
