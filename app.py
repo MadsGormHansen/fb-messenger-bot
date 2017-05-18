e51f584deca8ccd0a7448dfb4f0eb29227949d58
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-05-18 14:23:41 +0200

    Update

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 print os.environ['APP_SETTINGS']
 
+db = SQLALchemy(app)
+
 @app.route('/', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
