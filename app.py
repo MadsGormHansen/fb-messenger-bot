dcfc87dd96f6bf135be9a10546cedaaf3ebfca56
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-06-29 13:14:17 +0200

    message

--- app.py
+++ app.py
@@ -21,15 +21,13 @@ def verify():
     
     return "Hello world", 200
 
-
 @app.route('/', methods=['POST'])
 def webhook():
     page.handle_webhook(request.get_data(as_text=True))
-    return "ok"
+    print "ok"
 
 Kom_i_gang = ("Kom igang")
-Velkomst_send = ("Hej og velkommen til Interflora! Mit navn er Flora og jeg er din automatiske chatbot som vil hjælpe dig med at finde det helt rigtige. Fortæl først, hvad du kigger efter, fx blomster, chokolade, vin eller gavepakker.")
-
+Velkomst_send = ("hej")
 eftervelkomst_receive1 = ("købe", "se", "undersøge", "sende", "tænke", "tænkte", "hjælpe", "hjælp", "har", "bruge", "have", "finde", "sende")
 eftervelkomst_receive2 = ("blomster", "buketter", "flot", "blomst", "buket") 
 eftervelkomst_receive3 = ("alkohol", "gin", "rom", "vodka", "cognac", "vin", "øl", "smag")
@@ -48,7 +46,7 @@ eftervelkomst_send3 = ("hvem tænker du at give en gave", "hvem ønsker du at gi
 
 person_detect = ("mor", "mors", "far", "fars" , "kæreste", "kærestes", "kone", "kones", "sambo", "forældre", "forældres", "medarbejder", "kollega", "teammate", "mor,", "far,", "kæreste,", "kone,", "forældre,")
 
-Anledning = ("jubilæum", "fødselsdag", "bryllup", "kobberbryllup", "guldbryllup", "sølvbryllup", "bryllupsdag", "farsdag","morsdag" ,"begravelse", "sygdom", "fordi", "fylder")
+Anledning = ("jubilæum", "fødselsdag", "bryllup", "kobberbryllup", "guldbryllup", "sølvbryllup", "bryllupsdag", "farsdag", "morsdag" ,"begravelse", "sygdom", "fordi", "fylder")
 
 Andledning_send = ("hvilken anledning gives der blomster til?")
 
