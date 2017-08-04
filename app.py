3861d578c9043363a68c78d13da669da4c8b41a9
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-08-04 10:36:54 +0200

    commit

--- app.py
+++ app.py
@@ -199,7 +199,6 @@ def received_message(event):
         page.send(sender_id, blomster_url)
     else: page.send(sender_id, reply_text)
     
-    db.session.add(Result(sender_id,message,reply_text))
     print(reply_text)
     print(message)
 
