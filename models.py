19f2488e62fa2f977333a42dd0ae92e08135b988
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-08-15 15:17:51 +0200

    update

--- models.py
+++ models.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from app import db
+
+app = Flask(__name__)
+
+app.secret_key= "MA1114ha"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://mgh:Analytics4ever1@responsive-sandbox.cloudapp.net/SMP?driver=SQL+Server+Native+Client+11.0'
+
+db= SQLAlchemy(app)
 
 #Base = declarative_base()
 #engine= create_engine('mssql+pyodbc://mgh:Analytics4ever1@responsive-sandbox.cloudapp.net/SMP?driver=SQL+Server+Native+Client+11.0')
