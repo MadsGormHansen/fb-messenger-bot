9bca83333e2d99807cab6e5523203eb3dc1bdc40
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-08-15 15:16:32 +0200

    update

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ app = Flask(__name__)
 
 page = Page(os.environ["PAGE_ACCESS_TOKEN"])
 
+app.secret_key= "MA1114ha"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://mgh:Analytics4ever1@responsive-sandbox.cloudapp.net/SMP?driver=SQL+Server+Native+Client+11.0'
+
+db= SQLAlchemy(app)
+
 #connection = engine.connect()
 #connection.add(Result("1234567", "dfdfasfd", "fadkleelle"))
 #connection.close()
--- models.py
+++ models.py
@@ -1,19 +1,11 @@
 from flask import Flask
-#from sqlalchemy.ext.declarative import declarative_base 
-from sqlalchemy import create_engine, Column, Integer, String
-
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from app import db
 
 #Base = declarative_base()
 #engine= create_engine('mssql+pyodbc://mgh:Analytics4ever1@responsive-sandbox.cloudapp.net/SMP?driver=SQL+Server+Native+Client+11.0')
 
-app.secret_key= "MA1114ha"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://mgh:Analytics4ever1@responsive-sandbox.cloudapp.net/SMP?driver=SQL+Server+Native+Client+11.0'
-
-db= SQLAlchemy(app)
-
-class Result(Base):
+class Result(db.Model):
     __tablename__ = 'Results'
 
     id = db.Column(db.Integer, primary_key=True)
