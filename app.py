8f493e55281efbbc2a3cd5808b831055946c6b95
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-08-16 13:14:26 +0200

    update

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ import sys
 import json
 import random
 from flask import Flask, request
+from models import db,Result
 from flask_sqlalchemy import SQLAlchemy
 from fbmq import Attachment, Template, QuickReply, Page
 
 app = Flask(__name__)
 
-#app.secret_key= "MA1114ha"
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://mgh:Analytics4ever1@responsive-sandbox.cloudapp.net/SMP?driver=SQL+Server+Native+Client+11.0'
+app.secret_key= "MA1114ha"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://mgh:Analytics4ever1@responsive-sandbox.cloudapp.net/SMP?driver=SQL+Server+Native+Client+11.0'
 
-#db = SQLAlchemy(app)
-
-#db.session.add(Result("11111222","heeheh","hidfj"))
-#db.session.commit()
+db.session.connect()
+db.session.add(Result("11111222","heeheh","hidfj"))
+db.session.commit()
+db.session.connection.close()
 #connection = engine.connect()
 #connection.add(Result("1234567", "dfdfasfd", "fadkleelle"))
 #connection.close()
--- db_create.py
+++ db_create.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from models import Result
+from models import db, Result
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,15 +8,15 @@ app.secret_key= "MA1114ha"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://mgh:Analytics4ever1@responsive-sandbox.cloudapp.net/SMP?driver=SQL+Server+Native+Client+11.0'
 
-db = SQLAlchemy(app)
+
 #engine = app.create_engine("mssql+pyodbc://mgh:Analytics4ever1@responsive-sandbox.cloudapp.net/SMP?driver=SQL+Server+Native+Client+11.0")
 
 #receved_senderid= received_message.get('sender_id')
 #print(receved_senderid)
 #db.session.add(Result(receved_senderid,"hej", "hej"))
-#db.session.commit()
 
-db.session.add(Result("1111222","heeheh","hidfj"))
+
+db.session.add(Result("222","dfed","aefdfd"))
 db.session.commit()
 #db.session.add(Result("123534", "hej hej", "hfuhfeaijdfo"))
 
