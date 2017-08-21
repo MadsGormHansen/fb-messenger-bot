97d626feadc8ff140d203107330bf935d8fcd88c
Mads Gorm Hansen <madsgormhansen@gmail.com>
2017-08-21 11:45:01 +0200

    update

--- db_create.py
+++ db_create.py
@@ -1,21 +1,17 @@
 import os
+import inspect
 from flask import Flask
-from models import db, Result
-from app import received_message
+from flask_sqlalchemy import SQLAlchemy
+from models import Result
+from app import app, db, received_message
 from fbmq import Page
 
-app = Flask(__name__)
-
 app.secret_key= "MA1114ha"
+app.config.from_object(os.environ["PAGE_ACCESS_TOKEN"])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://mgh:Analytics4ever1@responsive-sandbox.cloudapp.net/SMP?driver=SQL+Server+Native+Client+11.0'
 
-page = Page(os.environ["PAGE_ACCESS_TOKEN"])
-
-@page.handle_message
-def import(event):
-    sender_id = event.sender_id
-    vars(sender_id)
-    text_file = open("testsheet.txt", "w")
-    text_file.write(var)
-    text_file.close()
+code = inspect.getsource(received_message)
+print code
+#db.session.add(Result("1234","hjehj","dfhafei"))
+#db.seesion.commit()
